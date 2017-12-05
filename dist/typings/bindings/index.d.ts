f4400a11fdc1e0f0b2f441876cd489fc0a78127f
Sergey Tokarev <sergey.tokarev@rd-dev.net>
2017-12-05 19:30:37 +0200

    Create new bindings on each reactBindings call

--- dist/typings/bindings/index.d.ts
+++ dist/typings/bindings/index.d.ts
@@ -1,5 +1,5 @@
 export * from "./configuration";
 import { AnyFormBindingConfiguration } from "./configuration";
 import { FieldBindingConfiguration } from "./field-binding-configuration";
-export declare const reactBinding: AnyFormBindingConfiguration;
+export declare function reactBinding(): AnyFormBindingConfiguration;
 export declare function field(): FieldBindingConfiguration;
--- src/bindings/index.ts
+++ src/bindings/index.ts
@@ -3,7 +3,9 @@ import { AnyFormBindingConfiguration } from "./configuration";
 import { FieldBindingConfiguration } from "./field-binding-configuration";
 import { FormBindingConfiguration } from "./form-binding-configuration";
 
-export const reactBinding: AnyFormBindingConfiguration = new FormBindingConfiguration();
+export function reactBinding(): AnyFormBindingConfiguration {
+    return new FormBindingConfiguration();
+}
 
 export function field(): FieldBindingConfiguration {
     return new FieldBindingConfiguration();
